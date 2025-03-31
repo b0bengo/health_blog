6df5094146c4eeb9a8d873612cb3c2474bdd9195
Alex <alex_p97@live.com>
2025-03-31 11:59:11 +0100

    Created homepage

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+class Homepage(ListView):
+    """
+    This view handles the homepage of the blog. It uses ListView to display a list of blog posts.
+    """
+    model = Post
+    template_name = 'index.html'
